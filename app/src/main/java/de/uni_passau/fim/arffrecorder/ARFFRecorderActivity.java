1b86dd675a0ec0d9f1271c11023665584905d1cb
matfax <matthias.fax@gmail.com>
2017-05-13 10:16:58 +0200

    Remove unnecessary options menu

--- app/src/main/java/de/uni_passau/fim/arffrecorder/ARFFRecorderActivity.java
+++ app/src/main/java/de/uni_passau/fim/arffrecorder/ARFFRecorderActivity.java
@@ -147,17 +147,6 @@ public class ARFFRecorderActivity extends AppCompatPreferenceActivity {
         public void onCreate(Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
             addPreferencesFromResource(R.xml.pref_general);
-            setHasOptionsMenu(true);
-        }
-
-        @Override
-        public boolean onOptionsItemSelected(MenuItem item) {
-            int id = item.getItemId();
-            if (id == android.R.id.home) {
-                startActivity(new Intent(getActivity(), ARFFRecorderActivity.class));
-                return true;
-            }
-            return super.onOptionsItemSelected(item);
         }
     }
 }
